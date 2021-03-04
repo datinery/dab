8be18b85be02a12783a7cb72524ecf54736f141d
Henry Cho <hscho817@gmail.com>
2021-03-04 16:26:38 +0000

    refactor: structure

--- lib/dab.dart
+++ lib/dab.dart
@@ -1,6 +1,6 @@
 library dab;
 
 export 'src/dds/dds.dart';
-export 'src/exception/handled_exception.dart';
+export 'src/exception/exception.dart';
 export 'src/http_client/http_client.dart';
 export 'src/bootstrap/bootstrap.dart';
---
+++ lib/src/exception/exception.dart
@@ -0,0 +1,1 @@
+export 'handled_exception.dart';
